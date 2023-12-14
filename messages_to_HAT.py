3c3ade90b74af255f8260781bf33935bff8053be
leantonucci <54019142+leantonucci@users.noreply.github.com>
2023-12-14 05:39:41 -0300

    Update messages_to_HAT.py

--- messages_to_HAT.py
+++ messages_to_HAT.py
@@ -1,6 +1,7 @@
 import serial
 import struct
 import time
+import datetime
 
 def send_ascii_read_int32(port, ascii_text):
     with serial.Serial(port, baudrate=9600, timeout=1) as ser:
@@ -44,7 +45,7 @@ def send_hex_receive_decimal(port, hex_data):
 
 # Example usage:
 # Replace 'COM1' with your serial port name (e.g., 'COM3' for Windows).
-port_name = 'COM5'
+port_name = 'COM6'
 
 # Example sending 'hello' and reading int32
 int_value = send_ascii_read_int32(port_name, ",T1'")
@@ -54,13 +55,32 @@ print(f"Received int32 value: {int_value}")
 value = send_ascii_read_uint16(port_name, ",I1'")
 print(f"Received uint16 value: {value}")
 
+######## READING ########
+
+print(datetime.datetime.now())
+
 print(f"TEMP 1: ")
 send_ascii_receive_decimal(port_name, ",T1'")
+print(f"TEMP 2: ")
+send_ascii_receive_decimal(port_name, ",T2'")
 print(f"PROPORTIONAL PART: ")
 send_ascii_receive_decimal(port_name, ",P1'")
 print(f"INTEGRAL PART: ")
 send_ascii_receive_decimal(port_name, ",I1'")
 print(f"DERIVATIVE PART: ")
 send_ascii_receive_decimal(port_name, ",D1'")
+print(f"Setpoint: ")
+send_ascii_receive_decimal(port_name, ",S1'")
+print("A1: ")
+send_ascii_receive_decimal(port_name, ",A1'")
+
+####### WRITING ############3
+
+############ CHANGE MODE OF OPERATION ################
 #send_ascii_receive_binary(port_name, ",b1' 5 CR")
-#send_hex_receive_decimal(port_name, "69 31 00 3C 0D")
+########### CHANGE PROPORTIONAL ##############
+#send_hex_receive_decimal(port_name, "70 31 23 28 0D")
+########## CHANGE INTEGRAL ###############
+#send_hex_receive_decimal(port_name, "69 31 00 78 0D")
+########## CHANGE DERIVATIVE ###############
+#send_hex_receive_decimal(port_name, "64 31 00 00 0D")
